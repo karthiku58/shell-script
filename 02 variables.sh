7812bc54c15d371b9a21c498f63f0d095577b6e7
karthiku <ukarthik278@gmail.com>
2026-03-10 18:52:43 +0530

    script

---
+++ 02 variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the username"
+
+read USERNAME #here USERNAME is variable
+
+echo "Please enter password:"
+
+read PASSWORD
+
+echo "Username is: $USERNAME,Password is $PASSWORD"
