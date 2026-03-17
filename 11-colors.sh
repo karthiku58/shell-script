af4654aa2bae91b996150dfc891de622d11d33b8
karthiku <ukarthik278@gmail.com>
2026-03-17 14:25:58 +0530

    script

--- 11-colors.sh
+++ 11-colors.sh
@@ -3,13 +3,17 @@
  TIMESTAMP=$(date +%F-%H-%M-%S)
  SCRIPT_NAME=$(echo  $0 | cut -d "." -f1)
  LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+ echo "script started executing at :" $TIMESTAMP
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[0m"
  VALIDATE(){
     if [$1 -ne 0]
     then
-        echo "$2...Failure"
+        echo "$2...$R Failure $N"
         exit
     else
-        echo "$2...success"
+        echo "$2...$G success $N"
     fi
  }
 
@@ -20,8 +24,11 @@
 else
     echo "you are super user"
 fi
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installation Mysql"
 
-dnf install git -y
-VALIDATE $? "Installing Mysql"
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
+
+dnf install dockerr -y &>>$LOGFILE
+VALIDATE $? "Installing docker"
